1710289dcfc22419faec134a0153303c527384b3
luckkun <luckkun123@163.com>
2021-10-08 21:42:06 +0800

    修改

--- src/main/java/com/zk/springboot/Application.java
+++ src/main/java/com/zk/springboot/Application.java
@@ -6,14 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@MapperScan("com.zk.springboot.modular.mapperni ") // 扫描mapper类，把bean加入到spring容器管理
+@MapperScan("com.zk.springboot.*.mapper") // 扫描mapper类，把bean加入到spring容器管理
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("程序启动成功！");
-        System.out.println("本地调试文档地址：http://localhost:8881/swagger-ui.htmll");
+
+        //System.out.println("本地调试文档地址：http://localhost:8881/swagger-ui.html");
+        //由于我使用了swagger第三方ui：swagger-bootstrap-ui，所以我直接访问url：http://localhost:8881/doc.html
+        System.out.println("本地调试文档地址：http://localhost:8881/doc.html");
     }
 
 }
